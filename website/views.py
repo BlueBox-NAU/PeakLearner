d05e1d3ee74b89c117e702b02b706432a7ce0be1
JonathanHillman <jonathandalehillman@gmail.com>
2021-02-10 15:42:05 -0700

    added GET functionality

--- website/views.py
+++ website/views.py
@@ -36,15 +36,32 @@ def registerPOST(request):
 
     email = request.params['email']
     password = request.params['password']
-    username = request.params['username']
+    secret = request.params['secret']
 
     user = User(email, password)
-    user.put(username)
+    user.put(secret)
     test = user.get()
     
-    print(test)
     url = request.route_url('login')
     return HTTPFound(location=url)
+
+@view_config(route_name = 'account', renderer = "account.html")
+def account(request):
+    return{}
+
+@view_config(route_name='login', renderer = "account.html", request_method = "POST")
+def loginPOST(request):
+
+    print(request.matchdict)
+    email = request.params['email']
+    print(email)
+    password = request.params['password']
+
+    user = User(email, password)
+    secret = user.get()
+    
+    return {'secret': secret}
+
 
 @view_config(route_name='login', renderer='login.html')
 def login(request):
